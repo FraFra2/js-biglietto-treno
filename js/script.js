57f726bbc2b5eff4a2c926e4445dbd5fff3e6625
FraFra2 <kekkino41@gmail.com>
2023-06-12 18:30:51 +0200

    Calcolo Finale Prezzo con sconti

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 //Html elements
-const ageLabel = document.querySelector("#user-age")
-const distanceLabel = document.querySelector("#user-distance")
-const titleLabel = document.querySelector("h1")
-const finalPricelabel = document.querySelector("#final-price")
+const ageLabel = document.querySelector("#user-age");
+const distanceLabel = document.querySelector("#user-distance");
+const titleLabel = document.querySelector("h1");
+const finalPricelabel = document.querySelector("#final-price");
 
+//Variables
+let price, finalPrice;
+let discount, discountValue; 
+let discountedPrice;
 
 //User Inputs
-const userAge = prompt("Inserire la propria età:")
+const userAge = prompt("Inserire la propria età:");
 
-const userDistance = prompt("Inserire la distanza da percorrere:")
+const userDistance = prompt("Inserire la distanza da percorrere:");
 
 if (isNaN(userAge) || isNaN(userDistance)){
     titleLabel.innerHTML = "Non è stato inserito un numero, riprovare."
@@ -16,14 +20,23 @@ if (isNaN(userAge) || isNaN(userDistance)){
 else{
     ageLabel.innerHTML = `Età inserita: ${userAge}`;
     distanceLabel.innerHTML = `Distanza da percorrere: ${userDistance}km`;
+    price = userDistance * 0.21
 }
-
-
 
 if(userAge < 18){
-        
+    discount = 20;
+}
+else if(userAge > 65){
+    discount = 40;
+}
+else{
+    discount = 0;
 }
 
+discountValue = price * discount / 100;
+finalPrice = price - discountValue;
+
+finalPricelabel.innerHTML = `Prezzo del biglietto: ${finalPrice.toFixed(2)}€`;
 
 
 
